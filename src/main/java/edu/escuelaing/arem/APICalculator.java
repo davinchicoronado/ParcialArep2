1d373552fc899bed6800acec04bbeb356c8e616a
David Coronado <david.coronado@escuelaing.edu.co>
2021-03-24 15:23:59 -0500

    addSparkServer

---
+++ src/main/java/edu/escuelaing/arem/APICalculator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem;
+
+
+/**
+ *
+ * @author David Coronado
+ */
+public class APICalculator {
+    
+    
+    public double calculateCos(double value){
+        return Math.cos(value);
+    
+    }
+    public double calculateLog(double value){
+    
+        return Math.log(value);
+    }
+    
+
+    
+}
---
+++ src/main/java/edu/escuelaing/arem/SparkWebServer.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem;
+
+import spark.Request;
+import spark.Response;
+import static spark.Spark.*;
+
+
+/**
+ *
+ * @author David Coronado
+ */
+public class SparkWebServer {
+    public static void main(String[] args) {
+        port(getPort());
+        
+       
+        get("/cos", (req, res) -> inputDataPage(req, res));
+        get("/log", (req, res) -> inputDataPage2(req, res));
+
+        
+    }
+    private static String inputDataPage(Request req, Response res) {
+        
+        Double num = Double.parseDouble(req.queryParams("number"));
+        APICalculator calculadora = new APICalculator();
+        
+        res.type("application/json");
+        
+       String pageContent=
+
+                "{"+
+                   " \"operation\": \"cos\","+
+                "\"input\":"+ num +
+                 " \"output\":" +calculadora.calculateCos(num) +
+                "}";
+       return pageContent;
+       
+        
+    }
+    
+    private static String inputDataPage2(Request req, Response res) {
+        Double num = Double.parseDouble(req.queryParams("number"));
+        APICalculator calculadora = new APICalculator();
+        res.type("application/json");
+        
+       String pageContent=
+                "{"+
+                   " \"operation\": \"cos\","+
+                "\"input\":"+ num +
+                 " \"output\":" +calculadora.calculateLog(num) +
+                "}";
+
+        return pageContent;
+    
+    
+    }
+
+    
+
+    private static int getPort() {
+            if (System.getenv("PORT") != null) {
+                    return Integer.parseInt(System.getenv("PORT"));
+            }
+            return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
+ }
+
+}
